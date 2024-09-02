1cc80bc992f0fcb6f469e00a4f90f3bd1987291d
corrado85 <77513385+corrado85@users.noreply.github.com>
2024-09-02 10:52:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,21 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-st.write('Ciao Ila!')
+st.title('Report Interattivo con Streamlit')
+
+# Controlli utente
+valore = st.sidebar.slider('Seleziona un valore', 0, 100, 50)
+opzione = st.sidebar.selectbox('Seleziona una colonna', ['colonna1', 'colonna2', 'colonna3'])
+
+# Calcoli
+risultato = valore * 2
+st.write(f'Il doppio di {valore} è {risultato}')
+
+# Grafico interattivo
+fig, ax = plt.subplots()
+x = np.linspace(0, 10, 100)
+y = np.sin(x) * valore
+ax.plot(x, y)
+st.pyplot(fig)
