2081f38ffaa6425b4dd21374afe3e07bf5496276
corrado85 <77513385+corrado85@users.noreply.github.com>
2024-11-22 12:55:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,13 @@ quadrato = numero ** 2
 
 # Visualizzazione del risultato
 st.write(f"Il quadrato di {numero} è {quadrato}")
+
+
+import yfinance as yf
+from datetime import date, timedelta
+
+today = date.today()
+df = yf.download(tickers=symbol, start=today, end=today + timedelta(days=1))['Adj Close']
+
+
+st.write(f"\n\nIl prezzo di bitcoin oggi è{df}")
