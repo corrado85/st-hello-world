d4c7af88219cde73ec538245a019e330737dc328
corrado85 <77513385+corrado85@users.noreply.github.com>
2024-11-22 14:41:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,10 @@ st.write(f"Il quadrato di {numero} è {quadrato}")
 
 
 
+
 import yfinance as yf
 from datetime import date, timedelta
 import streamlit as st
-
 
 # Simbolo del ticker
 symbol = "BTC-USD"
@@ -26,43 +26,26 @@ symbol = "BTC-USD"
 # Data odierna
 today = date.today()
 
-# Scarica i dati
-df = yf.download(tickers=symbol, start=today, end=today + timedelta(days=1))['Adj Close']
+# Scarica i dati per il prezzo odierno
+df_today = yf.download(tickers=symbol, start=today, end=today + timedelta(days=1))['Adj Close']
 
-# Mostra il risultato su Streamlit
-st.write(f"Il prezzo di oggi di bitcoing è {round(df.iloc[-1,0])}")
+# Mostra il prezzo odierno di Bitcoin
+st.write(f"Il prezzo di oggi di Bitcoin è {round(df_today.iloc[-1])}")
 
-
-
-
-
-# Impostazioni iniziali
+# Impostazioni iniziali per il grafico temporale
 start_date = "2020-01-01"  # Data di inizio
 end_date = date.today().strftime('%Y-%m-%d')  # Data di fine: oggi
 interval = "1mo"  # Intervallo mensile
 
-# Scarica i dati da Yahoo Finance
-df = yf.download(tickers=symbol, start=start_date, end=end_date, interval=interval)['Adj Close'].to_period('M')
-
-
-
+# Scarica i dati storici da Yahoo Finance
+df_historical = yf.download(tickers=symbol, start=start_date, end=end_date, interval=interval)['Adj Close'].to_period('M')
 
 # Converti il DataFrame in una forma adatta per il grafico
-df_reset = df.reset_index()  # Ripristina l'indice temporale in una colonna normale
+df_reset = df_historical.reset_index()  # Ripristina l'indice temporale in una colonna normale
 
 # Usa la colonna 'Date' per l'asse X e 'Adj Close' per l'asse Y
 st.line_chart(df_reset[['Date', 'Adj Close']].set_index('Date'))
 
-
-
-
-
-# Visualizza il grafico su Streamlit
+# Visualizza il grafico su Streamlit con il titolo
 st.title(f"Grafico Temporale dei Prezzi di {symbol}")
-st.line_chart(df)
-
-
-
-
-
-
+st.line_chart(df_historical)
